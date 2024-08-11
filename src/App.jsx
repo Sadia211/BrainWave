af26e8de028c63ac5e57f7033f0366e882b40f6c
Sadia211 <sadiakhanam121@gmail.com>
2024-08-11 12:55:29 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Button from './components/Button.jsx'; // Make sure this path is correct
+import ButtonGradient  from './assets/svg/ButtonGradient'
+import Header from './components/Header.jsx';
+import Hero from './components/Hero.jsx';
+import Benefits from './components/Benefits.jsx';
+const App = () => {
+  return (
+    <>
+      
+      <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+       <Header/>
+       <Hero/>
+       <Benefits/>
+      </div>
+      <ButtonGradient />
+    </>
+  );
+};
+
+export default App;
